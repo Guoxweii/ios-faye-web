9a888a428abbccd857d9fd3b50bf18a53fc1d65a
gxw <alphaguoxiongwei@gmail.com>
2013-11-14 18:07:16 +0800

    增加ios客户端

---
+++ faye-ios/faye-ios/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  faye-ios
+//
+//  Created by gxw on 13-11-14.
+//  Copyright (c) 2013年 gxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ faye-ios/faye-iosTests/faye_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  faye_iosTests.m
+//  faye-iosTests
+//
+//  Created by gxw on 13-11-14.
+//  Copyright (c) 2013年 gxw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface faye_iosTests : XCTestCase
+
+@end
+
+@implementation faye_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
