599c0ed6c58ab616af03c80eff226f9b8514b526
gxw <alphaguoxiongwei@gmail.com>
2013-11-14 19:04:20 +0800

    基本结构完成 可完整运行

--- faye-ios/faye-ios/ViewController.m
+++ faye-ios/faye-ios/ViewController.m
@@ -7,23 +7,94 @@
 //
 
 #import "ViewController.h"
+#import "FayeClient.h"
 
-@interface ViewController ()
-
+@interface ViewController () <FayeClientDelegate>
+@property (nonatomic, strong) FayeClient *client;
 @end
 
 @implementation ViewController
 
+- (void)loadView{
+    [super loadView];
+    
+    UIView *view=[[UIView alloc]initWithFrame:[UIScreen mainScreen].applicationFrame];
+    
+    view.alpha=0.5;
+    view.backgroundColor=[UIColor cyanColor];
+    self.view = view;
+    
+    self.label = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 200)];
+    [self.view addSubview:self.label];
+    self.label.text = @"receving message.....";
+    
+    NSLog(@"分辨率: %@",  [[UIScreen mainScreen] bounds]);
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _client = [[FayeClient alloc] initWithURLString:@"ws://localhost:9292/faye" channel:@"/foo"];
+    _client.delegate = self;
+    [_client connectToServer];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [_client disconnectFromServer];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - Faye Client Delegate
+- (void)connectedToServer
+{
+    NSLog(@"Connected to server");
+}
+
+- (void)disconnectedFromServer
+{
+    NSLog(@"Disconnected from server");
+}
+
+- (void)subscriptionFailedWithError:(NSString *)error
+{
+    NSLog(@"Subscription did fail: %@", error);
+}
+
+- (void)subscribedToChannel:(NSString *)channel
+{
+    NSLog(@"Subscribed to channel: %@", channel);
+}
+
+- (void)messageReceived:(NSDictionary *)messageDict channel:(NSString *)channel
+{
+    NSString *string = [NSString stringWithFormat:@"messageReceived %@ by channel: %@",messageDict, channel];
+    NSLog(string);
+    self.label.text = string;
+}
+- (void)connectionFailed
+{
+    NSLog(@"Connection Failed");
+}
+- (void)didSubscribeToChannel:(NSString *)channel
+{
+    NSLog(@"didSubscribeToChannel %@", channel);
+    [_client sendMessage:@{@"jayztemplier" : @"Hello World! everyone!"} onChannel:@"/foo"];
+}
+- (void)didUnsubscribeFromChannel:(NSString *)channel
+{
+    NSLog(@"didUnsubscribeFromChannel %@", channel);
+}
+- (void)fayeClientError:(NSError *)error
+{
+    NSLog(@"fayeClientError %@", error);
+}
+
+
 
 @end
